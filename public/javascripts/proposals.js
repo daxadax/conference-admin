0c98aca9e58b5fd198e569a39810358253fd17db
dax <d.dax@email.com>
2020-02-12 22:14:34 +0100

    Add intro text to proposals form

--- public/javascripts/proposals.js
+++ public/javascripts/proposals.js
@@ -1,25 +1,27 @@
 $(document).ready(function() {
   // move to the next section
-  $('#proposal-form-content .form-group .btn-next').on('click', function(){
-    var section = $(this).parent('.form-group')
-    if ( !section.find(":input").valid() ) {
+  $('#proposal-form-content .form-stage .btn-next').on('click', function(){
+    var section = $(this).parent('.form-stage')
+        position = section.data('position')
+
+    if ( position != 1 && !section.find(":input").valid() ) {
       $('label.error').addClass('text-warning');
       return
     }
 
-    var nextSection = parseInt(section.data(('position')) + 1)
+    var nextSection = parseInt(position) + 1
 
-    $('#proposal-form-content .form-group').addClass('hidden');
-    $('.form-group[data-position='+ nextSection +']').removeClass('hidden');
+    $('#proposal-form-content .form-stage').addClass('hidden');
+    $('.form-stage[data-position='+ nextSection +']').removeClass('hidden');
   });
 
   // move to the previous section
-  $('#proposal-form-content .form-group .btn-prev').on('click', function(){
-    var section = $(this).parent('.form-group').data('position');
+  $('#proposal-form-content .form-stage .btn-prev').on('click', function(){
+    var section = $(this).parent('.form-stage').data('position');
     var prevSection = parseInt(section) - 1
 
-    $('#proposal-form-content .form-group').addClass('hidden');
-    $('.form-group[data-position='+ prevSection +']').removeClass('hidden');
+    $('#proposal-form-content .form-stage').addClass('hidden');
+    $('.form-stage[data-position='+ prevSection +']').removeClass('hidden');
   });
 
   // trigger change event on range input
