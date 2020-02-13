6cebd28cae0cc4f0ffa3f5432a62e89497a42d01
dax <d.dax@email.com>
2020-02-13 18:45:46 +0100

    Move inline javascript into javascript file

--- public/javascripts/proposals.js
+++ public/javascripts/proposals.js
@@ -31,3 +31,62 @@ $(document).ready(function() {
   });
 });
 
+function updateProposalType(value) {
+  $('.experience-variant').addClass('hidden');
+
+  if ( $.inArray(value, ['lecture', 'workshop']) != -1 ) {
+    var headingText = 'Previous experience';
+        previousExperienceText = 'How experienced of a public speaker are you?';
+        portfoloText = 'Please link to any podcasts, conference videos or other media which demonstrates your presentation skills'
+
+    $('#previous-experience-wrapper').removeClass('hidden');
+    $('label#previous-experience').html(previousExperienceText);
+  } else if ( $.inArray(value, ['ritual', 'performance']) != -1 ) {
+    var headingText = 'Previous experience';
+        action = (value == 'ritual') ? 'conducting rituals' : 'putting on this performance'
+        previousExperienceText = 'How experienced are you in '+action+'?'
+        portfoloText = 'Please link to any media which can help us understand your work'
+
+    $('#previous-experience-wrapper').removeClass('hidden');
+    $('label#previous-experience').html(previousExperienceText);
+  } else if ( value == 'other' ) {
+    var headingText = 'More information'
+        portfoloText = 'Please link to any media which can help us understand your work'
+
+    $('#portfolio-description').removeClass('hidden');
+  } else {
+    var headingText = 'Portfolio';
+        portfoloText = 'Please provide links to your portfolio'
+
+    $('#previous-experience-wrapper').addClass('hidden');
+    $('#portfolio-description').removeClass('hidden');
+  }
+
+  $('#conditional-information').html(headingText);
+  $('label#portfolio').html(portfoloText);
+}
+
+// TODO: set to an env variable
+function characterizeExperience(value) {
+  var experienceMap = {
+    1: "I've never done this before",
+    2: 'I have a bit of experience',
+    3: "I feel totally comfortable, but I'm not a pro",
+    4: 'I have a lot of experience',
+    5: 'I consider this my main occupation'
+  }
+
+  $('#experience-output').html(experienceMap[value]);
+}
+
+function submitProposal() {
+  var form = $('#proposal-form-content form')
+
+  form.validate();
+  $.post( "/proposal", form.serialize(), function() {
+    window.location = '/proposal_submitted'
+  });
+
+  return false;
+}
+
